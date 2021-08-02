b203129b14d77b0a4bca3d1d243d6c0b4dbb9c76
Hemant Pandey <hpandey110301@gmail.com>
2021-08-03 04:03:13 +0530

    Add classes and timeslots data for IT-A1 and refactor code

--- main.go
+++ main.go
@@ -25,9 +25,8 @@ func init() {
 	if err != nil {
 		log.Fatalf("Invalid bot parameters: %v", err)
 	}
-	bot.AddHandler(ready)
-	bot.AddHandler(messageCreate)
-	bot.Identify.Intents = discordgo.IntentsGuildMessages
+	addHandlers()
+
 }
 
 func main() {
@@ -53,17 +52,11 @@ func ready(s *discordgo.Session, r *discordgo.Ready) {
 	log.Println("Bot is up!")
 }
 
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	// Ignore all messages created by the bot itself
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
-	// If the message is "ping" reply with "Pong!"
-	if m.Content == "ping" {
-		s.ChannelMessageSend(m.ChannelID, "Pong!")
-	}
-	// If the message is "pong" reply with "Ping!"
-	if m.Content == "pong" {
-		s.ChannelMessageSend(m.ChannelID, "Ping!")
-	}
+func addHandlers() {
+	bot.AddHandler(ready)
+	bot.AddHandler(messageCreate)
+}
+
+func addIntents() {
+	bot.Identify.Intents = discordgo.IntentsGuildMessages
 }
---
+++ message_create.go
@@ -0,0 +1,18 @@
+package main
+
+import "github.com/bwmarrin/discordgo"
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore all messages created by the bot itself
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+	// If the message is "pong" reply with "Ping!"
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
+}
